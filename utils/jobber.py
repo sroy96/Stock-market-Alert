528f0b116cc1bfdf44854828da61fbfe82b56391
sroy96 <sauravroy.eee@gmail.com>
2020-11-10 23:42:25 +0530

    clean up

--- utils/jobber.py
+++ utils/jobber.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('../')
 from datetime import datetime
 import pprint
 from service.watch_tower import WatchTower
@@ -12,16 +10,18 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 def ring_jobber():
     stock_list = cache_util.create_cache_client().get(common_constants.CACHE_KEY)
-    print(type(stock_list))
-    date_time = datetime.now()
-    my_file = open('jobber_logs.txt', 'a')
-    my_file.write(f'\n=== === === SCHEDULE JOB RUNNING At {date_time} === === ===')
-    for _stocks in stock_list:
-        pprint.pprint(_stocks, indent=4)
-    watch_tower_obj = WatchTower(stock_list)
-    watch_tower_obj.business_logic()
+    if len(stock_list) > 0:
+        date_time = datetime.now()
+        my_file = open('jobber_logs.txt', 'a')
+        my_file.write(f'\n=== === === SCHEDULE JOB RUNNING At {date_time} and load size {len(stock_list)} === === ===')
+        for _stocks in stock_list:
+            pprint.pprint(_stocks, indent=4)
+        watch_tower_obj = WatchTower(stock_list)
+        watch_tower_obj.business_logic()
+    else:
+        pass
 
 
 while True:
     ring_jobber()
-    time.sleep(300)
+    time.sleep(180)
